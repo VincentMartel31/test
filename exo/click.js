a5411c3685cfcb5f976e1cb5a4f12acd89b09728
Vincent Martel <martelvincent175@gmail.com>
2024-11-13 10:47:21 +0100

    toggle

--- exo/click.js
+++ exo/click.js
@@ -2,22 +2,46 @@
  * Selectionne le premier élément h1
  * @type {HTMLElement}
  */
-let lesTxt = document.querySelector("h1");
+//let lesTxt = document.querySelector("h1");
 
 /**
  * Stocke la valeur du h1 dans une variable 
  * @type {string}
  */
-let valeurInitiale = lesTxt.innerText;
+//  let valeurInitiale = lesTxt.innerText   ;
 
 /**
  * Ajoute un event listener sur le click de l'element h1
  *Puis il switche de valeur lors du click 
  */
+/*
 lesTxt.addEventListener('click', () => {
-  if (lesTxt.innerText === "--😀--") {
-    lesTxt.innerText = valeurInitiale;
-  } else {
-    lesTxt.innerText = "--😀--";
-  }
+  lesTxt.innerText === "--😀--" ? lesTxt.innerText = valeurInitiale : lesTxt.innerText = "--😀--";
 });
+
+
+let leH1 = document.querySelector('#mainTitle');
+let selectTitle = false;    //! Un booléen pour savoir si le titre a été cliqué ou non
+
+leH1.addEventListener('click',()=>{
+    console.log('ok ca click');
+    leH1.innerText = selectTitle ? '-- 🥳 --':'D.O.M Events'; //? condition ternaire si selectTitle est vrai alors smiley sinon dom Events
+    selectTitle = !selectTitle; // ? à chaque click le booléen passe à son inverse (pour faire le re click)
+});
+*/
+
+let monh1 = document.getElementById('maintitle');
+let lien1 = document.getElementById('lien1');
+let lien2 = document.getElementById('lien2');
+let lien3 = document.getElementById('lien3');
+
+lien1.addEventListener("click",()=>{
+    monh1.classList.add('color');
+})
+lien2.addEventListener("click",()=>{
+    monh1.classList.remove('color');
+})
+lien3.addEventListener("click",()=>{
+    monh1.classList.toggle('color');
+})
+
